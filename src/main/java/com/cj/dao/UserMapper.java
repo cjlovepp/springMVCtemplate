7d3c18723f55b4380302e20f57a33fc5ea708d95
cj <cjlovepp@gmail.com>
2017-02-15 17:30:20 +0800

    集成mybatis

--- src/main/java/com/cj/dao/UserMapper.java
+++ src/main/java/com/cj/dao/UserMapper.java
@@ -1,12 +1,14 @@
 package com.cj.dao;
 
 import com.cj.entity.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * Created by chenjing on 2017/2/14.
  */
+@Repository
 public interface UserMapper {
     int addUser(User user);
 
